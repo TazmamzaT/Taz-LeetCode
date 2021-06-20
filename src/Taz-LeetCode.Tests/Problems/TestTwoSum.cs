c727fdb152ea83f8f7210b04202633010e198d39
TazmamzaT <TazmamzaT@gmail.com>
2021-06-20 22:34:46 +0000

    I didn't actually commit

--- src/Taz-LeetCode.Tests/Problems/TestTwoSum.cs
+++ src/Taz-LeetCode.Tests/Problems/TestTwoSum.cs
@@ -4,13 +4,38 @@ using Taz_LeetCode.Problems;
 public class TestTwoSum
 {
     [Fact]
-    public void ReturnsNull()
+    public void Example1()
     {
-    //Given
+        //Given
         TwoSum sut = new TwoSum();
-    //When
-        var result = sut.Solution(null, 0);
-    //Then
-        
+        //When
+        var result = sut.Solution(new int[] {2,7,11,15}, 9);
+        //Then
+        Assert.Equal(0, result[0]);
+        Assert.Equal(1, result[1]);
+    }
+
+    [Fact]
+    public void Example2()
+    {
+        //Given
+        TwoSum sut = new TwoSum();
+        //When
+        var result = sut.Solution(new int[] {3,2,4}, 6);
+        //Then
+        Assert.Equal(1, result[0]);
+        Assert.Equal(2, result[1]);
+    }
+
+    [Fact]
+    public void Example3()
+    {
+        //Given
+        TwoSum sut = new TwoSum();
+        //When
+        var result = sut.Solution(new int[] {3,3}, 6);
+        //Then
+        Assert.Equal(0, result[0]);
+        Assert.Equal(1, result[1]);
     }
 }
