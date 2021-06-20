5089728aaf4859443af254d86fa4e43e2dc2b173
TazmamzaT <TazmamzaT@gmail.com>
2021-06-20 02:48:00 +0000

    Working

---
+++ src/Taz-LeetCode.Tests/Problems/TestTwoSum.cs
@@ -0,0 +1,15 @@
+using Xunit;
+using Taz_LeetCode.Problems;
+
+public class TestTwoSum
+{
+    [Fact]
+    public void ReturnsNull()
+    {
+    //Given
+        TwoSum sut = new TwoSum();
+    //When
+        var result = sut.Solution(null);
+    //Then
+    }
+}
