d84610bd9a7725527f036e0fd6b72188fbd60db0
TazmamzaT <TazmamzaT@gmail.com>
2021-06-20 01:53:37 +0000

    Inital commit

---
+++ src/Taz-LeetCode.Problems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Taz_LeetCode.Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This solution is a container for the LeetCode solutions.");
+        }
+    }
+}
